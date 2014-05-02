2332885a12de831c125dbab3b63f80c559703f57
Lazarus404 <lee.sylvester@gmail.com>
2014-05-02 19:21:06 +0100

    Added gen_server impl for simpler usage

--- src/s3_xirsys.erl
+++ src/s3_xirsys.erl
@@ -4,6 +4,10 @@
 %% External exports
 %%--------------------------------------------------------------------
 -export([
+  init/2,
+  init/4,
+  init/5,
+  init/7,
 	create_user/2,
 	create_user/3,
 	create_user/4
@@ -11,6 +15,28 @@
 
 -include_lib("erlcloud/include/erlcloud.hrl").
 -include_lib("erlcloud/include/erlcloud_aws.hrl").
+
+%% --------------------------------------------------------------------------------------
+%% Defines
+%% --------------------------------------------------------------------------------------
+
+-define(DEFAULT_HOST, "localhost").
+-define(DEFAULT_PORT, 8080).
+-define(DEFAULT_PROXY_HOST, "localhost").
+-define(DEFAULT_PROXY_PORT, 8080).
+
+
+init(Id, Key) ->
+  init(Id, Key, ?DEFAULT_HOST, ?DEFAULT_PORT, "http", ?DEFAULT_PROXY_HOST, ?DEFAULT_PROXY_PORT).
+
+init(Id, Key, Host, Port) ->
+  init(Id, Key, Host, Port, "http", ?DEFAULT_PROXY_HOST, ?DEFAULT_PROXY_PORT).
+
+init(Id, Key, Host, Port, Protocol) ->
+  init(Id, Key, Host, Port, Protocol, ?DEFAULT_PROXY_HOST, ?DEFAULT_PROXY_PORT).
+
+init(Id, Key, Host, Port, Protocol, Proxy_Host, Proxy_Port) ->
+  erlcloud_s3:new(Id, Key, Host, Port, Protocol, Proxy_Host, Proxy_Port, []).
 
 -type s3_bucket_acl() :: private
                        | public_read
@@ -36,4 +62,9 @@ create_user(Email, UserName, ACL, Config) ->
               end,
     Data = list_to_binary(io_lib:format("{\"email\": \"~s\", \"name\": \"~s\"}", [Email, UserName])),
     POSTData = {Data, "application/json"},
-    erlcloud_s3:s3_request(Config, post, "", "/riak-cs/user", "", [], POSTData, Headers).
+    case erlcloud_s3:s3_request(Config, post, "", "/riak-cs/user", "", [], POSTData, Headers) of
+      {_Header, Body} ->
+        Body;
+      E ->
+        E
+    end.
